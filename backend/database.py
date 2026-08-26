aa99690729718710e13047f43c02f5cc1134ae32
unknown <harishragavendharpgms@gmail.com>
2026-08-26 20:34:12 +0530

    Add production database link

--- backend/database.py
+++ backend/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 # ⚠️ VERY IMPORTANT: Look at the line below! 
 # You need to replace YOUR_PASSWORD with the password you created when installing PostgreSQL.
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin123@localhost:5432/farmflow_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://farmflow_db_h8w7_user:5FqywwEofQu07S0pFsiMA7gZBscE8JF3@dpg-da7fneek1f9s73d98cmg-a/farmflow_db_h8w7"
 
 # This creates the "engine" that actually talks to PostgreSQL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
